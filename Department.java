93005984f8227bb50e1c6f81aa92305506eb85aa
01fe22bca057 <samuelkotikela14@gmail.com>
2024-03-10 22:47:23 +0530

    Dept

---
+++ Department.java
@@ -0,0 +1,16 @@
+class Department {
+    public void getDeptName(int deptId) {
+        if(deptId==1)
+        {
+            System.out.println("Computer Science");
+        }
+        else if(deptId==2)
+        {
+            System.out.println("BCA");
+        }
+        else if(deptId==3){
+            System.out.println("MCA");
+        }
+        
+    }
+}
